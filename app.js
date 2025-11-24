0e83bb64e425cbad3ed7230aa30718c39a93972d
DharmnathShinde <surajshinde7888@gmail.com>
2025-11-24 12:14:10 +0530

    Update CORS settings in app.js to change allowed origin to the new production server URL for consistency in deployment.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = socketio(server, {
   cors: {
-    origin: "https://cricket-auction-one.vercel.app",
+    origin: "https://cricket-auction-6bnc.vercel.app",
     credentials: true,
   },
 });
@@ -29,7 +29,7 @@ const io = socketio(server, {
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://cricket-auction-one.vercel.app",
+    origin: "https://cricket-auction-6bnc.vercel.app",
     credentials: true,
   })
 );
